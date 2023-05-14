de9f475a14bbf5442b95578effdc36de25abc04c
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-05-14 16:48:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Polityka prywatności i warunki korzystania z serwisu</title>
+  </head>
+  <body>
+    <h1>Warunki korzystania z serwisu</h1>
+    <ol>
+      <li>Serwis jest przeznaczony tylko dla użytkowników, którzy ukończyli 13 lat.</li>
+      <li>Zakazuje się używania serwisu w celach nielegalnych.</li>
+      <li>Wszelkie naruszenia praw autorskich w jakikolwiek sposób są zabronione.</li>
+      <li>Zastrzegamy sobie prawo do zablokowania dostępu użytkownikom, którzy łamią nasze warunki korzystania z serwisów.</li>
+    </ol>
+
+    <h1>Polityka prywatności</h1>
+    <ol>
+      <li>
+        Dane osobowe<br />
+        Serwis nie zbiera żadnych informacji, które mogą identyfikować użytkowników, chyba że dobrowolnie udostępnią je nam poprzez nasz serwis.
+      </li>
+      <li>
+        Dane techniczne<br />
+        Korzystając z naszej aplikacji/bota, zbieramy pewne informacje techniczne, takie jak adres IP użytkownika, typ przeglądarki, system operacyjny itp. Te informacje służą jedynie do analizowania trendów i do zarządzania serwerami.
+      </li>
+      <li>
+        Bezpieczeństwo<br />
+        Zabezpieczamy dane użytkowników stosując odpowiednie technologie i procedury bezpieczeństwa. Nie mamy jednak kontroli nad działaniami osób trzecich, które mogą próbować uzyskać dostęp do naszych systemów, ale zapewniamy, że podejmujemy wszelkie środki ostrożności, aby chronić dane użytkowników.
+      </li>
+      <li>
+        Zmiany w polityce prywatności<br />
+        Zastrzegamy sobie prawo do zmiany polityki prywatności w dowolnym momencie. W przypadku zmiany polityki, powiadomimy użytkowników o zmianach.
+      </li>
+      <li>
+        Kontakt<br />
+        W przypadku jakichkolwiek pytań dotyczących naszej polityki prywatności, prosimy o kontakt z nami poprzez naszego serwer na discord.
+      </li>
+    </ol>
+  </body>
+</html>
